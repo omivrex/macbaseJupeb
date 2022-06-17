a5be7bd5e5767c79653f130866bf47b4e235c202
RexOmiv <omivrex@gmail.com>
2022-06-17 17:55:11 +0100

    integrated firebase into project

---
+++ utils/firebaseInit.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app"
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+import 'firebase/compat/database'
+import {FIREBASE_PROJECT_KEY, MESSAGING_SENDER_ID} from '@env'
+
+const app = firebase.initializeApp({
+    apiKey: FIREBASE_PROJECT_KEY,
+    authDomain: "jupebmacbase.firebaseapp.com",
+    projectId: "jupebmacbase",
+    storageBucket: "jupebmacbase.appspot.com",
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: "1:271272994760:web:5ba6bdf9c329aa2dbcc2fd",
+    measurementId: "G-YFE7Q5GGLW"
+});
+
+const analytics = getAnalytics(app);
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+export const rtdb = firebase.database()
+// export const storage = firebase.storage()
