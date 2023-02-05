2a01e4f505a058d7a13eae4e3fe51fdc0a1d9e6b
RexOmiv <omivrex@gmail.com>
2023-02-05 22:54:04 +0100

    added zooom feature to answer component

--- components/Reusable/Answer.component.js
+++ components/Reusable/Answer.component.js
@@ -76,7 +76,7 @@ export default function ({data, extraStyles}) {
                 <MathJax
                     html={
                         `   <head>
-                                <meta name="viewport"  content="width=device-width, initial-scale=1.0 maximum-scale=1.0">
+                                <meta name="viewport"  content="width=device-width, initial-scale=1.0 ">
                             </head>
                             <body>
                                 <style>
